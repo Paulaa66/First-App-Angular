48046f7c4a7b1ea0a9b151617938382a2e3e9fba
Paulaa66 <111074405+Paulaa66@users.noreply.github.com>
2025-07-31 20:39:01 +0300

    add new files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Home } from '../components/home/home';
+import { About } from '../components/about/about';
+import { Portfolio } from '../components/portfolio/portfolio';
+import { Contact } from '../components/contact/contact';
+import { Notfound } from '../components/notfound/notfound';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: Home, title: 'Home Page' },
+  { path: 'about', component: About, title: 'About Page' },
+  { path: 'portfolio', component: Portfolio, title: 'Portfolio Page' },
+  { path: 'contact', component: Contact, title: 'Contact Page' },
+  { path: '**', component: Notfound, title: 'Not Found' },
+];
